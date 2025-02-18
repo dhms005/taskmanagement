9f934ffa6f8bee6bb6bbdcee294e8e08a5b644da
dharmesh.parmar <dharmesh.parmar@digi-corp.com>
2025-02-18 11:56:43 +0530

    task list UI done

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -6,21 +6,17 @@ import 'package:taskmanagement/viewmodels/selected_task_provider.dart';
 import 'package:taskmanagement/viewmodels/task_provider.dart';
 import 'package:taskmanagement/viewmodels/user_preferences_provider.dart';
 import 'package:taskmanagement/views/add_task_screen.dart';
-import 'package:taskmanagement/views/edit_task_screen.dart';
 import 'package:taskmanagement/views/settings_screen.dart';
-import 'package:taskmanagement/widgets/task_item.dart';
+import 'package:taskmanagement/widgets/task_list.dart';
 
 class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final tasks = ref.watch(taskProvider);
-
-    // Get screen width to differentiate between mobile and tablet.
-    double screenWidth = MediaQuery.of(context).size.width;
 
     final tasks = ref.watch(taskProvider);
     final selectedTask = ref.watch(selectedTaskProvider);
     final userPreferences = ref.watch(userPreferencesProvider);
+
     // Sort tasks based on the selected sort order
     List<Task> tasksToDisplay = [...tasks];
     if (userPreferences.sortOrder == 'byDate') {
@@ -46,9 +42,11 @@ class HomeScreen extends ConsumerWidget {
           ),
         ],
       ),
-      body: screenWidth < 600 // If the screen width is less than 600px (mobile)
-          ? _buildMobileView(tasksToDisplay, context, ref, selectedTask)
-          : _buildTabletView(tasksToDisplay, context, ref, selectedTask),
+      body: TaskList(
+          tasks: tasksToDisplay,
+          context: context,
+          ref: ref,
+          selectedTask: selectedTask),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -58,71 +56,6 @@ class HomeScreen extends ConsumerWidget {
         },
         child: const Icon(Icons.add),
       ),
-    );
-  }
-
-  // Mobile View: A simple list of tasks
-  Widget _buildMobileView(List<Task> tasks, BuildContext context, WidgetRef ref,
-      Task? selectedTask) {
-    return tasks.isEmpty
-        ? Center(child: Text("No tasks available"))
-        : ListView.builder(
-            itemCount: tasks.length,
-            itemBuilder: (context, index) {
-              final task = tasks[index];
-              return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditTaskScreen(task: task),
-                      ),
-                    );
-                    selectedTask == task;
-                    ref.read(selectedTaskProvider.notifier).selectTask(task);
-                  },
-                  child: TaskItem(task: task, context: context, ref: ref));
-            },
-          );
-  }
-
-  // Tablet View: Split view with list and details side-by-side
-  Widget _buildTabletView(List<Task> tasks, BuildContext context, WidgetRef ref,
-      Task? selectedTask) {
-    print(selectedTask?.title);
-    return Row(
-      children: [
-        // Task List
-        Expanded(
-          flex: 2,
-          child: tasks.isEmpty
-              ? Center(child: Text("No tasks available"))
-              : ListView.builder(
-                  itemCount: tasks.length,
-                  itemBuilder: (context, index) {
-                    final task = tasks[index];
-                    return GestureDetector(
-                        onTap: () {
-                          selectedTask == task;
-                          ref.read(selectedTaskProvider.notifier).selectTask(task);
-                        },
-                        child:
-                            TaskItem(task: task, context: context, ref: ref));
-                  },
-                ),
-        ),
-        // Divider between the task list and task details
-        VerticalDivider(width: 1),
-        // Task Details (This could be a placeholder or actual details)
-        Expanded(
-          flex: 3,
-          child: Center(
-            child: selectedTask == null
-                ? Center(child: Text("Select a task to view details"))
-                : EditTaskScreen(key: ValueKey(selectedTask.id), task: selectedTask),
-          ),
-        ),
-      ],
     );
   }
 }
--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:taskmanagement/models/task_model.dart';
-import 'package:taskmanagement/viewmodels/task_provider.dart';
+import 'package:taskmanagement/viewmodels/selected_task_provider.dart';
 import 'package:taskmanagement/views/edit_task_screen.dart';
 import 'package:taskmanagement/widgets/task_item.dart';
 
@@ -9,15 +9,29 @@ class TaskList extends StatelessWidget {
   List<Task> tasks;
   BuildContext context;
   WidgetRef ref;
+  Task? selectedTask;
 
   TaskList(
       {super.key,
       required this.tasks,
       required this.context,
-      required this.ref});
+      required this.ref,
+      this.selectedTask});
 
   @override
   Widget build(BuildContext context) {
+
+    // Get screen width to differentiate between mobile and tablet.
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return screenWidth < 600 // If the screen width is less than 600px (mobile)
+        ? _buildMobileView(tasks, context, ref, selectedTask)
+        : _buildTabletView(tasks, context, ref, selectedTask);
+  }
+
+  // Mobile View: A simple list of tasks
+  Widget _buildMobileView(List<Task> tasks, BuildContext context, WidgetRef ref,
+      Task? selectedTask) {
     return tasks.isEmpty
         ? Center(child: Text("No tasks available"))
         : ListView.builder(
@@ -32,9 +46,52 @@ class TaskList extends StatelessWidget {
                         builder: (context) => EditTaskScreen(task: task),
                       ),
                     );
+                    ref.read(selectedTaskProvider.notifier).selectTask(task);
                   },
                   child: TaskItem(task: task, context: context, ref: ref));
             },
           );
+  }
+
+  // Tablet View: Split view with list and details side-by-side
+  Widget _buildTabletView(List<Task> tasks, BuildContext context, WidgetRef ref,
+      Task? selectedTask) {
+    print(selectedTask?.title);
+    return Row(
+      children: [
+        // Task List
+        Expanded(
+          flex: 2,
+          child: tasks.isEmpty
+              ? Center(child: Text("No tasks available"))
+              : ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks[index];
+                    return GestureDetector(
+                        onTap: () {
+                          ref
+                              .read(selectedTaskProvider.notifier)
+                              .selectTask(task);
+                        },
+                        child:
+                            TaskItem(task: task, context: context, ref: ref));
+                  },
+                ),
+        ),
+        // Divider between the task list and task details
+        VerticalDivider(width: 1),
+        // Task Details (This could be a placeholder or actual details)
+        Expanded(
+          flex: 3,
+          child: Center(
+            child: selectedTask == null
+                ? Center(child: Text("Select a task to view details"))
+                : EditTaskScreen(
+                    key: ValueKey(selectedTask.id), task: selectedTask),
+          ),
+        ),
+      ],
+    );
   }
 }
